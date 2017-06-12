3492453cdac2a8f18ac8b5dcbdc3315225d4c9da
Andre Holzner <holzner@lpcmssd02.dyndns.cern.ch>
2017-06-12 16:22:51 +0200

    added a test case for FlowchartCase6 where a pseudofed goes into warning

--- src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase6Test.java
+++ src/test/java/rcms/utilities/daqexpert/reasoning/logic/failures/FlowchartCase6Test.java
@@ -35,4 +35,28 @@ public class FlowchartCase6Test extends FlowchartCaseTestBase {
 
 	}
 
+	@Test
+	public void case2Test() throws URISyntaxException {
+	  // UTC:  Fri,  9 Jun 2017 21:16:17 UTC
+	  // CEST: Fri,  9 Jun 2017 23:16:17 CEST
+		DAQ snapshot = getSnapshot("1497042977243.smile");
+
+		assertEqualsAndUpdateResults(false, fc1,snapshot);
+		assertEqualsAndUpdateResults(false, fc2,snapshot);
+		assertEqualsAndUpdateResults(false, fc3,snapshot);
+
+		// new subcases of old flowchart case 4
+		assertEqualsAndUpdateResults(false, piDisconnected,snapshot);
+		assertEqualsAndUpdateResults(false, piProblem,snapshot);
+		assertEqualsAndUpdateResults(false, fedDisconnected,snapshot);
+		assertEqualsAndUpdateResults(false, fmmProblem,snapshot);
+
+		assertEqualsAndUpdateResults(false, fc5,snapshot);
+		assertEqualsAndUpdateResults(true, fc6,snapshot);
+
+		assertEqualsAndUpdateResults(false, unidentified, snapshot);
+
+	}
+
+
 }
